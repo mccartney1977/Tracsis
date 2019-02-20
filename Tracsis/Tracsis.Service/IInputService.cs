b85da8cf3427dbcb0f6876e468c11b88ccb4d10b
Matthew McCartney <matthew.joe.mccartney@hotmail.com>
2019-02-20 20:28:31 +0700

    Add input service

--- Tracsis/Tracsis.Service/IInputService.cs
+++ Tracsis/Tracsis.Service/IInputService.cs
@@ -2,8 +2,12 @@
 
 namespace Tracsis.Service
 {
+    /// <summary>
+    /// Service to handle code inputs from a text file.
+    /// </summary>
     public interface IInputService
     {
         IEnumerable<string> GetInputCodes(string filePath);
+        bool HasCodes { get; }
     }
 }
--- Tracsis/Tracsis.Service/InputService.cs
+++ Tracsis/Tracsis.Service/InputService.cs
@@ -1,14 +1,41 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace Tracsis.Service
 {
+    /// <summary>
+    /// Service to read input codes from a text file.
+    /// </summary>
     public class InputService : IInputService
     {
+        private List<string> _InputCodes { get; set; }
+
+        public InputService()
+        {
+            _InputCodes = new List<string>();
+        }
+
+        /// <summary>
+        /// Read the inout file and place all codes into a list.
+        /// </summary>
+        /// <param name="filePath"></param>
+        /// <returns></returns>
         public IEnumerable<string> GetInputCodes(string filePath)
         {
             throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// If the list contains any data return true.
+        /// </summary>
+        public bool HasCodes
+        {
+            get
+            {
+                return _InputCodes.Count() > 0;
+            }
         }
     }
 }
